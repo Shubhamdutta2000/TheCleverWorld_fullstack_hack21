410bd8b09ef3a2e3b3773754d0e1ea93533e95eb
Prasundas99 <prasundas115@gmail.com>
2021-06-25 16:25:44 +0530

    [Improvment] add reducer store and fix bug among them

---
+++ client/src/redux/action-types/user.js
@@ -0,0 +1,11 @@
+export const REGISTER_USER = 'register_user';
+export const REGISTER_USER_SUCCESS = 'register_user_success';
+export const REGISTER_USER_FAILURE = 'register_user_failure';
+
+export const LOGIN_USER = 'login_user';
+export const LOGIN_USER_SUCCESS = 'login_user_success';
+export const LOGIN_USER_FAILURE = 'login_user_failure';
+
+export const LOGOUT_USER = 'logout_user';
+
+ 
